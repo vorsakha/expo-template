f3a0628806249b99c89f3c5438224586cd6d48b1
vorsakha <andreitf.dev@gmail.com>
2022-08-06 11:09:48 -0300

    refactor: fix navigation type declaration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,96 +5,16 @@ import { AppRoutes } from './app.routes';
 declare global {
   namespace ReactNavigation {
     interface RootParamList {
-      SignIn: undefined;
-      SignUp: undefined;
-      Profile: undefined;
-      ManageSocial: undefined;
-      Playlist: undefined;
-      ProfileModal: undefined;
-      AuthModal?: {
-        trackId: number;
-      };
-      TrackPlayer: {
-        trackId?: number;
-        trackTitle?: string;
-        trackDescription?: string;
-        trackImageUrl?: string;
-        trackFileUrl?: string;
-        playlistImageUrl: string;
-        playlistTitle: string;
-        playlistDescription: string;
-        playlistId: number;
-      };
-      Tracks: {
-        playlistId?: number;
-        playlistCoverImageUrl?: string;
-        playlistTitle?: string;
-        playlistDescription?: string;
-      };
-      Rate: {
-        trackId?: number;
-        playlistImageUrl?: string;
-        playlistTitle?: string;
-        playlistDescription?: string;
-        playlistId?: number;
-      };
-      UseTerms?: {
-        title?: string;
-      };
-      PrivacyPolicy?: {
-        title?: string;
-      };
-      ForgotPassword?: {
-        title?: string;
-      };
+      Home: undefined;
     }
   }
 }
 
-export type ParamList = {
-  AuthModal: {
-    trackId: number;
-  };
-  SignIn: {
-    trackId: number;
-  };
-  SignUp: {
-    trackId: number;
-  };
-  Rate: {
-    trackId: number;
-    playlistImageUrl: string;
-    playlistTitle: string;
-    playlistDescription: string;
-    playlistId: number;
-  };
-  Tracks: {
-    playlistId: number;
-    playlistTitle: string;
-    playlistDescription: string;
-    playlistCoverImageUrl: string;
-  };
-  TrackPlayer: {
-    trackId: number;
-    trackTitle: string;
-    trackDescription: string;
-    trackImageUrl: string;
-    trackFileUrl: string;
-    playlistImageUrl: string;
-    playlistTitle: string;
-    playlistDescription: string;
-    playlistId: number;
-  };
-  UseTerms: {
-    title: string;
-  };
-  PrivacyPolicy: {
-    title: string;
-  };
-  ForgotPassword: {
-    title: string;
-  };
-};
+// export type ParamList = {
+//   Home: {
+//     eventualParam: string;
+//   };
+// };
 
 export function Routes() {
   return (
